ec897632ea47a55762ae394c33c80eae0a27a4e1
Arthur Furman <foarthur@gmail.com>
2021-07-02 09:46:25 +0300

    use reselect lib and add memoized selectors for cart items and items count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,16 +4,21 @@ import { connect } from "react-redux";
 import "./cart-icon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleCart } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
-const CartIcon = ({ toggleCart }) => (
+const CartIcon = ({ toggleCart, totalItemCount }) => (
   <div className="cart-icon" onClick={toggleCart}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{totalItemCount}</span>
   </div>
 );
+
+const mapStatetoProps = state => ({
+  totalItemCount: selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = dispatch => ({
   toggleCart: () => dispatch(toggleCart()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStatetoProps, mapDispatchToProps)(CartIcon);
